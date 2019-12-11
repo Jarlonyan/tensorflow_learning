c378601a6452d735ce13cbde96781a5ff07f14d2
Jarlonyan <286568450@qq.com>
2019-12-11 10:15:33 +0800

    update

--- wide_deep/main.py
+++ wide_deep/main.py
@@ -4,13 +4,16 @@ import seaborn as sns
 import pandas as pd
 #import warning
 
-_CSV_COLUMNS = [
-    'age', 'workclass', 'fnlwgt', 'education', 'education_num'
+_CSV_COLUMNS = [                                #定义CVS列名
+    'age', 'workclass', 'fnlwgt', 'education', 'education_num',
+    'marital_status', 'occupation', 'relationship', 'race', 'gender',
+    'capital_gain', 'capital_loss', 'hours_per_week', 'native_area',
+    'income_bracket'
 ]
-
+ 
 
 def main():
-    df = pd.read_csv("income_data/audlt.data.csv", names=_CSV_COLUMNS, skiprows=0, encoding="ISO-8859-1")
+    df = pd.read_csv("income_data/adult.data.csv", names=_CSV_COLUMNS, skiprows=0, encoding="ISO-8859-1")
     df.loc[df['income_bracket']=='<=50K', 'income_bracket'] = 0  #字段转化
     df.loc[df['income_bracket']=='>50K', 'income_bracket'] = 1   #字段转化
     df1 = df.dropna(how='all', axis=1)      #数据清洗：将空值去掉
