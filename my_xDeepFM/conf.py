e68d362791ab8cad39020c95b300517214dee5a3
Jarlonyan <286568450@qq.com>
2020-03-13 18:42:52 +0800

    update

---
+++ my_xDeepFM/conf.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+instance_train_file = 'data/instance_train.data'
+instance_valid_file = 'data/instance_valid.data'
+instance_test_file = 'data/instance_test.data'
+
+# train
+batch_size = 4096
+epochs = 4000
+learning_rate = 0.01
+
+
+
