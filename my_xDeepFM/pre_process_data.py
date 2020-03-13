101eae41593c8b8fa412305fbac6fde3297057c5
Jarlonyan <286568450@qq.com>
2020-03-13 15:49:53 +0800

    update

--- my_xDeepFM/pre_process_data.py
+++ my_xDeepFM/pre_process_data.py
@@ -10,52 +10,52 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 # features
-numeric_features = ['all_launch_count', 'last_launch', 'all_video_count', 'last_video', 'all_video_day', \
+Numeric_features = ['all_launch_count', 'last_launch', 'all_video_count', 'last_video', 'all_video_day', \
                     'all_action_count', 'last_action', 'all_action_day', 'register_day']
-single_features = ['register_type', 'device_type']
-multi_features = []
+Single_features = ['register_type', 'device_type']
+Multi_features = []
 
-num_embedding = True
-single_feature_frequency = 10
-multi_feature_frequency = 0
+Nnum_embedding = True
+Single_feature_frequency = 10
+Multi_feature_frequency = 0
 
 # model
 FM_layer = True
 DNN_layer = True
 CIN_layer = False
 
-use_numerical_embedding = False
+Uuse_numerical_embedding = False
 
-embedding_size = 16
-dnn_net_size = [128,64,32]
-cross_layer_size = [10,10,10]
-cross_direct = False
-cross_output_size = 1
+Embedding_size = 16
+Dnn_net_size = [128, 64, 32]
+Cross_layer_size = [10, 10, 10]
+Cross_direct = False
+Cross_output_size = 1
 
 # train
-batch_size = 4096
-epochs = 4000
-learning_rate = 0.01
+Batch_size = 4096
+Epochs = 4000
+Learning_rate = 0.01
 
-def get_dict():
+def get_dict(train, valid, test):
     global_emb_idx = 0
     num_dict = {}
     single_dict = {}
     multi_dict = {}
     backup_dict = {}
 
-    if num_features and num_embedding:
-        for s in num_features:
+    if Num_features and Num_embedding:
+        for s in Num_features:
+            #each field
             num_dict[s] = global_emb_idx
             global_emb_idx += 1
             #for NaN
             backup_dict[s] = global_emb_idx
             global_emb_idx += 1
-    #end-if
 
-    if single_features:
-        for s in single_features:
-            #eacho field
+    if Single_features:
+        for s in Single_features:
+            #each field
             frequency_dict = {}
             current_dict = {}
             values = pd.concat([train, valid, test])
@@ -65,7 +65,52 @@ def get_dict():
                 else:
                     frequency_dict[v] = 1
             for k,v in frequency_dict.items():
-                if v > single
+                if v > Single_feature_frequency:
+                    current_dict[k] = global_emb_idx
+                    global_emb_idx += 1
+            single_dict[s] = current_dict
+            backup_dict[s] = global_emb_idx
+    if Multi_features:
+        #each field
+        for s in Multi_features:
+            frequency_dict = {}
+            current_dict = {}
+            values = pd.concat([train, valid, test)[s]
+            for vs in values:
+                for v in vs.split('|'):
+                    v = int(v)
+                    if v in frequency_dict:
+                        frequency_dict[v] += 1
+                    else:
+                        frequency_dict[v] = 1
+            for k,v in frequency_dict.items():
+                if v>Multi_feature_frequency:
+                    current_dict[k] = global_emb_idx
+                    global_emb_ix += 1
+            multi_dict[s] = current_dict
+            backup_dict[s] = global_emb_idx
+
+    return global_emb_idx, num_dict, single_dict, multi_dict, backup_dict
+
+def trains_data(data, num_dict, single_dict, multi_dict, instance_file):
+    single_num = 0
+    multi_num = 0
+    with open(instance_file, 'w') as f:
+        #label, index:value
+        def instance_write_to_file(line):
+            label = line['label']
+            f.write(str(label)+',')
+            for s in Num_features:
+                now_v = line[s]
+                f.write(str(num_dict[s])+':'+str(now_v)+',')
+                single_num += 1
+            for s in Single_features:
+                now_v = line[s] 
+                if now_v in single_dict[s]:
+                    now_idx = 
+        #end-func
+        data.apply(lambda x: instance_write_to_file(x), axis=1)
+    #end-with
 
 def pre_process_data():
     label_num = 0
@@ -79,7 +124,7 @@ def pre_process_data():
     scalar = MinMaxScaler()
     all_data = pd.concat([train, valid, test])
 
-    for s in numeric_features:
+    for s in g_numeric_features:
         scalar.fit(all_data[s].values.reshape(-1,1))
         train[s] = scalar.transorm(tran[s].values.reshape(-1,1))
         valid[s] = scalar.transorm(valid[s].values.reshape(-1,1))
@@ -88,7 +133,12 @@ def pre_process_data():
         print 'error shape'
         return -1
 
-    num 
+    global_emb_idx, num_dict, single_dict, multi_dict, backup_dict = get_dict(train, valid, test)
+
+    trains_data(train, num_dict, single_dict, multi_dict, 'data/train_instance.data') 
+    trains_data(valid, num_dict, single_dict, multi_dict, 'data/valid_instance.data') 
+    trains_data(test, num_dict, single_dict, multi_dict, 'data/test_instance.data') 
+    
 
 
 def main():
