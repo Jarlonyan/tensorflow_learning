aec525ab047126a998eaefbaa4fc8e71f3d5f151
Jarlonyan <286568450@qq.com>
2020-04-27 18:23:33 +0800

    update

--- tf_introduction_principle/8_8_queue_coordinator.py
+++ tf_introduction_principle/8_8_queue_coordinator.py
@@ -11,4 +11,10 @@ images_test, labels_test = cifar10_input.inputs(eval_data=True, data_dir=data_di
 
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
-
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess, coord)
+    image_batch, label_batch = sess.run([images_test, labels_test])
+    print image_batch[0]
+    pylab.imshow(image_batch[0])
+    pylab.show()
+    coord.request_stop()
