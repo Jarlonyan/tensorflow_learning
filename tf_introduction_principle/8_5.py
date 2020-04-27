74e18caeb073c18bf2aacb5af565d48445a69725
yanzhenglong <yanzhenglong@bytedance.com>
2020-04-27 10:47:40 +0800

    update

---
+++ tf_introduction_principle/8_5.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+import tensorflow as tf
+import cifar10_input
+import pylab
+
+batch_size = 128
+
+
+
+
---
+++ tf_introduction_principle/8_9_cifar_convolution.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import sys
+sys.path.append('./CIFAR_TensorFlow/')
+import cifar10_input
+import tensorflow as tf
+import numpy as np
+import pylab
+
+batch_size = 128
+data_dir = 'CIFAR_TensorFlow/data/cifar-10-batches-bin'
+images_test, labels_test = cifar10_input.inputs(eval_data=True, data_dir=data_dir, batch_size=batch_size)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.train.start_queue_runners()
+image_batch, label_batch = sess.run([images_test, labels_test])
+pylab.imshow(image_batch[0])
+pylab.show()
+
+
