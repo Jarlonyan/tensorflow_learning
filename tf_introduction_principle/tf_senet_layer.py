9fec980142b0059888e3f4b9bf1f6ad1ae766f32
yanzhenglong <yanzhenglong@bytedance.com>
2020-10-09 10:32:19 +0800

    update

--- tf_introduction_principle/tf_senet_layer.py
+++ tf_introduction_principle/tf_senet_layer.py
@@ -13,7 +13,7 @@ def SENet_layer(embed_matrix, field_size, emb_size, pool_op, ratio):
         a = tf.layers.dense(z1, units=field_size, activation='relu')  #bs*field_size
         senet_embed = tf.multiply(embed_matrix, tf.expand_dims(a, axis=-1))   #(bs*field*emb) * (bs*field*1)
 
-    return senet_embed
+    return senet_embed, a
 
 def main():
     embed_table = tf.constant([[0,    0,    0   , 0   , 0,   0],
@@ -21,18 +21,22 @@ def main():
                                [-0.21, 0.22, 0.23, 0.24, -0.25, 0.26],
                                [0.31, 0.32, 0.33, 0.34, -0.35, 0.36],
                                [0.41, 0.42, 0.43, 0.44, -0.45, 0.46]], dtype=tf.float32)
+    field_size = 3
+    input_idx = tf.constant([2, 4, 3])
+    x1 = tf.identity(tf.nn.embedding_lookup(embed_table, input_idx))
 
-    input_idx = tf.constant([2, 4, 0, 3])
-    x = tf.identity(tf.nn.embedding_lookup(embed_table, input_idx))
+    input_idx = tf.constant([1, 3, 4])
+    x2 = tf.identity(tf.nn.embedding_lookup(embed_table, input_idx))
 
+    x = tf.stack([x1, x2], axis=0)
 
-    senet_embed_matrix = SENet_layer(x, 4, 6, 'avg', 0.2)
+    senet_embed_matrix, f_weight = SENet_layer(x, field_size, 6, 'avg', 0.2)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         print 'x=', sess.run(x)
         print 'senet_emb=', sess.run(senet_embed_matrix)
-    
+        print 'f_weight=', sess.run(f_weight)
 
 if __name__ == '__main__':
     main()
