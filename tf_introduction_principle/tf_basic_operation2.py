b7cd1d705c8375276dc5d96267ad3f789b20498d
yanzhenglong <yanzhenglong@bytedance.com>
2021-05-24 11:13:45 +0800

    update

--- tf_introduction_principle/tf_basic_operation2.py
+++ tf_introduction_principle/tf_basic_operation2.py
@@ -12,7 +12,6 @@ def tf_slice(sess):
     t1 = tf.slice(a, [1,0,0], [1,1,3])
     t2 = tf.slice(a, [1,0,0], [1,2,3])
     #t3 = tf.slice(a, [1,0,0], [2,3,1])
-
     print("t1=",sess.run(t1))
     print("t2=",sess.run(t2))
     #print("t3=",sess.run(t3))
@@ -29,15 +28,31 @@ def tf_split(sess):
 def tf_concat(sess):
     a = tf.constant([[1,2,3], [4,5,6]])
     b = tf.constant([[7,8,9], [10,11,12]])
-
     t = tf.concat([a,b], 0)
     print("t=", sess.run(t))
 
+#4. tf.transpose调换tensor的维度顺序，其实就是矩阵转置
+def tf_transpose(sess):
+    a = tf.constant([[1,2,3], [4,5,6]])
+    b = tf.transpose(a, perm=[1,0])
+    print("a=", sess.run(a))
+    print("a.transpose=", sess.run(b))
+
+#5. tf.pack是沿着某个维度pack
+def tf_stack(sess):
+    t1 = tf.constant([1,4])
+    t2 = tf.constant([2,5])
+    t3 = tf.constant([3,6])
+    a = tf.stack([t1, t2, t3])
+    print("tf.stack([t1,t2,t3])=", sess.run(a))
+
 def main():
     with tf.Session() as sess:
-        tf_slice(sess)
+        #tf_slice(sess)
         #tf_split(sess)
         #tf_concat(sess)
+        #tf_transpose(sess)
+        tf_stack(sess)
 
 if __name__ == '__main__':
     main()
