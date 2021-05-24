5faad2e70fefc9064cd9e51a9e31b801571c6e95
yanzhenglong <yanzhenglong@bytedance.com>
2021-05-24 10:20:32 +0800

    update

--- tf_introduction_principle/tf_basic_operation2.py
+++ tf_introduction_principle/tf_basic_operation2.py
@@ -8,12 +8,13 @@ tf.disable_v2_behavior()
 #1. tf.slice对tensor进行切片操作。begin表示从哪几个维度开始，size表示在input各个维度抽取的元素个数
 def tf_slice(sess):
     a = tf.constant([[[1,1,1], [2,2,2]],  [[3,3,3], [4,4,4]],  [[5,5,5], [6,6,6]]])
+    print("a.shape=", a.shape, ", a=", sess.run(a))
     t1 = tf.slice(a, [1,0,0], [1,1,3])
-    #t2 = tf.slice(a, [1,0,0], [1,2,3])
+    t2 = tf.slice(a, [1,0,0], [1,2,3])
     #t3 = tf.slice(a, [1,0,0], [2,3,1])
 
     print("t1=",sess.run(t1))
-    #print("t2=",sess.run(t2))
+    print("t2=",sess.run(t2))
     #print("t3=",sess.run(t3))
 
 #2. tf.split沿着某一个维度将tensor分割成xxx
@@ -34,9 +35,9 @@ def tf_concat(sess):
 
 def main():
     with tf.Session() as sess:
-        #tf_slice(sess)
+        tf_slice(sess)
         #tf_split(sess)
-        tf_concat(sess)
+        #tf_concat(sess)
 
 if __name__ == '__main__':
     main()
