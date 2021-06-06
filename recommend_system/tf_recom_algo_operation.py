976b0d7a8489b5e22b22509c4097e1bd83c3cd7f
yanzhenglong <yanzhenglong@bytedance.com>
2021-06-06 17:59:26 +0800

    update

--- recommend_system/tf_recom_algo_operation.py
+++ recommend_system/tf_recom_algo_operation.py
@@ -11,11 +11,10 @@ def embeddings(sess, slot_id):
     slotx_emb_table = tf.get_variable(name='multi_hot_embeds_slot_%s'%str(slot_id), shape=(100, embed_size), initializer=tf.glorot_uniform_initializer())
     #print("embeds=\n", sess.run(embeds))
 
-
     #slotx_index = tf.placeholder(dtype=tf.int64, shape=[None, 2])
     #slotx_value = tf.placeholder(dtype=tf.int64, shape=[None])
     slotx_index = tf.constant([[1,1], [2,2], [3,3]], dtype=tf.int64)
-    slotx_value = tf.constant([3,1,2], dtype=tf.int64)
+    slotx_value = tf.constant([0,1,2], dtype=tf.int64)
 
     print(slotx_emb_table.shape)
     print(slotx_index.shape)
@@ -28,32 +27,36 @@ def embeddings(sess, slot_id):
                     combiner="sum")
     return slotx_embed
 
-def get_senet_weights(embeddings):
+def get_senet_weights(sess, embeddings):
     slots_num = len(embeddings)
     inputs = []
     for embed in embeddings:
         inputs.append(tf.reduce_mean(embed, axis=1, keepdims=True))
     sequeeze_embedding = tf.concat(inputs, axis=1)
-    print("sequeeze_embedding.shape=",sequeeze_embedding.shape)
+    #print("sequeeze_embedding.shape=",sequeeze_embedding.shape)
 
     sequeeze_embedding = tf.layers.dense(inputs=sequeeze_embedding, units=32, activation=tf.nn.relu)
     weight_out = tf.layers.dense(inputs=sequeeze_embedding, units=2, activation=tf.nn.relu)    
+    sess.run(tf.global_variables_initializer())
+    print("weight_out=\n", sess.run(weight_out))
+
     return tf.split(weight_out, 2, axis=1)
 
-
 def main():
-    with tf.Session() as sess:
-        emb_slot1 = embeddings(sess, 1)
-        emb_slot2 = embeddings(sess, 2)
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            emb_slot1 = embeddings(sess, 1)
+            emb_slot2 = embeddings(sess, 2)
         
-        sess.run(tf.global_variables_initializer())
-        print("emb_slot1=\n", sess.run(emb_slot1))
-        print("emb_slot2=\n", sess.run(emb_slot2))
-        
-        res = get_senet_weights([emb_slot1, emb_slot2])
-        print("res0=\n", res[0])
-        print("res1=\n", res[1])
+            t0, t1 = get_senet_weights(sess, [emb_slot1, emb_slot2])
 
+            sess.run(tf.global_variables_initializer())
+            print("emb_slot1=\n", sess.run(emb_slot1))
+            print("emb_slot2=\n", sess.run(emb_slot2))        
+            print("t0=\n", sess.run(t0))
+            print("t1=\n", sess.run(t1))
+        #end-with
+    #end-with
 
 if __name__ == '__main__':
     main()
