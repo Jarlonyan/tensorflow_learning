b1da7e9a3ad2af845e08ba66fb9224b8c6fd73d6
yanzhenglong <yanzhenglong@bytedance.com>
2021-06-15 18:01:24 +0800

    update

--- recommend_system/tf_recom_algo_operation.py
+++ recommend_system/tf_recom_algo_operation.py
@@ -8,7 +8,7 @@ tf.disable_v2_behavior()
 g_emb_size = 4
 g_dict_len = 10
 
-
+#1. 简单的onehot embedding方式
 def onehot_embedding(sess, slot_id):
     slotx_emb_table = tf.get_variable(name="onehot_emb_slot%s"%str(slot_id), shape=(g_dict_len, g_emb_size), initializer=tf.glorot_uniform_initializer())
     slotx_index = tf.constant([2,1,3], dtype=tf.int64)
@@ -18,9 +18,11 @@ def onehot_embedding(sess, slot_id):
     print("emb(slot"+str(slot_id)+")=\n", sess.run(slotx_embed))
     return slotx_embed 
 
+#2. multihot embedding方式
 def multihot_embedding(sess, slot_id):
     slotx_emb_table = tf.get_variable(name='multi_hot_embeds_slot_%s'%str(slot_id), shape=(g_dict_len, g_emb_size), initializer=tf.glorot_uniform_initializer())
-    '''slotx_emb_table = tf.constant([[6.4, 1.2, 0.5, 3.3],
+    '''
+    slotx_emb_table = tf.constant([[6.4, 1.2, 0.5, 3.3],
                                    [0.3, 0.4, 0.5, 0.8],
                                    [1.5, 0.3, 2.2, 1.9],
                                    [0.4, 0.9, 1.1, 4.3]])
@@ -43,7 +45,32 @@ def multihot_embedding(sess, slot_id):
     print("emb(slot"+str(slot_id)+")=\n", sess.run(slotx_embed))
     return slotx_embed
 
+#2. 用item_emb对multihot做加权求和的attention
+def attention_func(Q, K, V):
+    Z = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], dtype=tf.float32)
+    Z = tf.divide(Z, tf.sqrt(dk))
+    Z = tf.nn.softmax(Z, dim=-1)
+    res = tf.matmul(Z, V)
+    res = tf.reduce_mean(res, axis=0)
+    return res
 
+def multihot_attention_embedding(sess, slot_id, batch_ids, item_emb):
+    slotx_emb_table = tf.get_variable(name='multi_hot_atten_embeds_slot_%s'%str(slot_id), shape=(g_dict_len, g_emb_size), initializer=tf.glorot_uniform_initializer())
+    sess.run(tf.global_variables_initializer())
+    batch_emb = []
+    Q = item_emb
+    for ids in batch_ids:
+        inds = tf.constant(ids, dtype=tf.int64)
+        V = tf.nn.embedding_lookup(slotx_emb_table, inds)
+        res = attention_func(Q, V, V)
+        batch_emb.append(res)
+        #print("res=", sess.run(V))
+    slot_emb =  tf.stack(batch_emb, axis=0)
+    print("emb(slot"+str(slot_id)+")=\n", sess.run(slot_emb))
+    return slot_emb
+
+#3. SENet
 def SENet(sess, embed_matrix, field_size, emb_size, ratio):
     z = tf.reduce_mean(embed_matrix, axis=2)  # bs*field*emb_size  ->  bs*field
     z1 = tf.layers.dense(z, units=field_size/ratio, activation='relu')
@@ -53,8 +80,9 @@ def SENet(sess, embed_matrix, field_size, emb_size, ratio):
     senet_embed = tf.multiply(embed_matrix, tf.expand_dims(w, axis=-1))   #(bs*field*emb) * (bs*field*1)
     return senet_embed, w
 
+#4. LHUCNet
 def mlp(sess, mlp_input, mlp_dims):
-    x = mlp_input
+    x = mlp_input # bs*d
     if len(mlp_dims) > 1:
         for idx,dim in enumerate(mlp_dims[0:-1]):
             x = tf.layers.dense(x, units=dim, activation='relu')
@@ -62,11 +90,10 @@ def mlp(sess, mlp_input, mlp_dims):
     sess.run(tf.global_variables_initializer())
     return x
 
-def lhuc_net(sess, lhuc_inputs, lhuc_dims, scale_last=False):
+def LHUCNet(sess, lhuc_inputs, lhuc_dims, scale_last=False):
     mlp_dims = [256, 256, 128, 64]
     cur_layer = lhuc_inputs
     for idx,dim in enumerate(mlp_dims[:-1]):
-        sess.run(tf.global_variables_initializer())
         lhuc_output = mlp(sess, lhuc_inputs, lhuc_dims+[int(cur_layer.shape[1])])
         lhuc_scale = 1.000 + 5.000 * tf.nn.tanh(0.200 * lhuc_output)
         cur_layer = mlp(sess, cur_layer*lhuc_scale, [dim])
@@ -79,12 +106,18 @@ def lhuc_net(sess, lhuc_inputs, lhuc_dims, scale_last=False):
     cur_layer = mlp(sess, cur_layer, [mlp_dims[-1]])
     return cur_layer
 
-
 def main():
     with tf.Graph().as_default():
         with tf.Session() as sess:
             emb_slot1 = onehot_embedding(sess, 1)
             emb_slot2 = multihot_embedding(sess, 2)
+
+            batch_ids = [ #batch_size=3,每一行表示一个user_recent序列
+                [5,2,6,1],
+                [3,2,5],
+                [6,8]
+            ]
+            emb_slot3 = multihot_attention_embedding(sess, 3, batch_ids, emb_slot1)
 
             '''
             #SENet
@@ -94,13 +127,14 @@ def main():
             print('f_weight=\n', sess.run(f_weight))
             '''
 
-            #'''
+            '''
             #LHUC
             lhuc_inputs = tf.concat([emb_slot1, emb_slot2], axis=1)
             lhuc_dims = [256, 256, 128, 64]
-            lhuc_output = lhuc_net(sess, lhuc_inputs, lhuc_dims, False)
+            lhuc_output = LHUCNet(sess, lhuc_inputs, lhuc_dims, False)
             print("lhuc_output=\n", sess.run(lhuc_output))
-            #'''
+            '''
+
 
         #end-with
     #end-with
