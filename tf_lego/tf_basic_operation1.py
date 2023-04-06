923abad2e26446a5b90e019c045d7ea7c6b840bd
yanzhenglong <yanzhenglong@bytedance.com>
2023-04-06 19:16:33 +0800

    update

--- tf_lego/tf_basic_operation1.py
+++ tf_lego/tf_basic_operation1.py
@@ -30,7 +30,6 @@ def tf_expand_dims(sess):
     print("b=\n", sess.run(b))
     print(t1.shape)
     print(t2.shape)
-    return
     t3 = tf.expand_dims(a, 2)
     t4 = tf.expand_dims(a, -1)
     print(t3.shape)
@@ -64,30 +63,80 @@ def tf_split(sess):
     a = tf.Variable([[[[1],[1],[1]], [[2],[2],[2]]]])
     #b = tf.split(a, num_or_size_splits ,axis=0)
 
-#6. tf.expand_dims，对tensor插入一个维度
-def tf_expand_dims(sess):
-    a = tf.constant([[1,2,3], [4,5,6]])
-    t1 = tf.expand_dims(a, 0)
-    t2 = tf.expand_dims(a, 1)
-    b = t1 - t2
-    print("t1=\n", sess.run(t1))
-    print("t2=\n", sess.run(t2))
-    print("b=\n", sess.run(b))
-    print(t1.shape)
-    print(t2.shape)
-    return
-    t3 = tf.expand_dims(a, 2)
-    t4 = tf.expand_dims(a, -1)
-    print(t3.shape)
-    print(t4.shape)
+#6. tf.transpose调换tensor的维度顺序，其实就是矩阵转置
+def tf_transpose(sess):
+    a = tf.constant([[1,2,3], [4,5,6]]) #shape=(2,3)
+    b = tf.transpose(a, perm=[1,0])
+    print("a=\n", sess.run(a))
+    print("tf.transpose(a,perm=[1,0])=\n", sess.run(b))
 
-#7. tf.linag.diag_part
+#7. tf.stack是沿着某个维度pack
+def tf_stack(sess):
+    t1 = tf.constant([1,4]) #shape=(2,)
+    t2 = tf.constant([2,5]) #shape=(2,)
+    t3 = tf.constant([3,6]) #shape=(2,)
+    a = tf.stack([t1, t2, t3])
+    b = tf.stack([t1, t2, t3], axis=1)
+    print("t1.shape=", t1.shape)
+    print("t1=", sess.run(t1))
+    print("t2=", sess.run(t2))
+    print("t3=", sess.run(t3))
+    print("tf.stack([t1,t2,t3])=\n", sess.run(a))         #shape=(3,2)
+    print("tf.stack([t1,t2,t3], axis=1)=\n", sess.run(b)) #shape=(2,3)
+
+#8. tf.unstack将输入的tensor按照指定的行或列进行拆分，并输出含有num个tensor的list
+def tf_unstack(sess):
+    a = tf.constant([[1,2,3], [4,5,6]]) #shape=(2,3)
+    b = tf.unstack(a, axis=1)  #所以拆分后有3个tensor
+    print("a=\n", sess.run(a))
+    print("tf.unstack(a,axis=1)(type=",type(b),")=>\n", b)
+    for x in b:
+        print (x)
+
+#9. tf.reverse沿着维度进行序列反转
+def tf_reverse(sess):
+    a = tf.constant([[1,2,3], [4,5,6]])
+    b = tf.reverse(a, [True, False])
+    print("a=\n", sess.run(a))
+    print("tf.reverse(a,[True,False])=\n", sess.run(b))
+
+#10. tf.gather根据indices所指示的参数获取tensor中的切片
+def tf_gather(sess):
+    a = tf.constant([11,22,33,44,55])
+    b = tf.gather(a, [2,0,3])
+    c = tf.gather(a, [1,4,2])
+    print("a=\n", sess.run(a))
+    print("tf.gather(a,[2,0,3])=\n", sess.run(b))
+    print("tf.gather(a,[1,4,2])=\n", sess.run(c))
+
+#11. tf.one_hot生成符合onehot编码的张量
+def tf_onehot(sess):
+    indices = [0,2,-1,1]  #要生成的张量
+    depth = 3             #在depth长度的数组中，哪个索引的值为onehot值
+    on_value = 1.0        #为onehot值时，该值为多少
+    off_value = 0.0       #非onehot值时，该值为多少
+    axis = -1             #axis=-1时，生成的shape=[indices长度,depth]。axis=0时，shape=[depth,indices长度]
+    a = tf.one_hot(indices, depth, on_value, off_value, axis)
+    print("indices=", indices)
+    print("tf.one_hot(indices, ...)=\n", sess.run(a))
+
+#12. tf.nn.softmax_cross_entropy_with_logits, softmax损失函数
+def tf_softmax_loss(sess):
+    labels = [[0,0,1], [0,1,0]]  #one-hot
+    logits = [[2,0.5,6], [0.1,0,3]]
+    res1 = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    print("res1.shape=", res1.shape)
+
+#13. tf.linag.diag_part
 def tf_diag_part(sess):
     logits = tf.constant([[1,1,1], [2,2,2], [3,3,3], [4,4,4], [5,5,5], [6,6,6]])
     sfx_prob = tf.linalg.diag_part(logits)
     print("sfx_prob=\n", sess.run(sfx_prob))
 
-
+#14. tf.eye，单位矩阵
+def tf_eye(sess):
+    a = tf.eye(5, 5, dtype=tf.int32)
+    print("tf.eye=\n", sess.run(a))
 
 def main():
     with tf.Session() as sess:
@@ -95,6 +144,16 @@ def main():
         tf_expand_dims(sess)
         #tf_squeeze(sess)
         #tf_tile(sess)
+        ##tf_split(sess)
+        #tf_transpose(sess)
+        #tf_stack(sess)
+        #tf_unstack(sess)
+        #tf_reverse(sess)
+        #tf_gather(sess)
+        #tf_onehot(sess)
+        #tf_softmax_loss(sess)
+        #tf_diag_part(sess)
+        #tf_eye(sess)
 
 if __name__ == '__main__':
     main()
--- tf_lego/tf_basic_operation2.py
+++ tf_lego/tf_basic_operation2.py
@@ -28,12 +28,7 @@ def tf_multiply(sess):
     c = tf.multiply(a, b)
     print("c=\n", sess.run(c))
 
-#4. tf.eye，单位矩阵
-def tf_eye(sess):
-    a = tf.eye(5, 5, dtype=tf.int32)
-    print("tf.eye=\n", sess.run(a))
-
-#5. tf.concat沿着某一维度连接tensor
+#4. tf.concat沿着某一维度连接tensor
 def tf_concat(sess):
     a = tf.constant([[1,2,3], [4,5,6]])
     b = tf.constant([[7,8,9], [10,11,12]])
@@ -42,27 +37,20 @@ def tf_concat(sess):
     print("b=\n", sess.run(b))
     print("tf.concat([a,b],0)=\n", sess.run(t))
 
-#6. tf.nn.softmax_cross_entropy_with_logits, softmax损失函数
-def tf_softmax_loss(sess):
-    labels = [[0,0,1], [0,1,0]]  #one-hot
-    logits = [[2,0.5,6], [0.1,0,3]]
-
-    res1 = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-    print("res1.shape=", res1.shape)
-
-#7. tf.equal，对比这两个tensor相等的元素，相等则返回True，否则返回False
+#5. tf.equal，对比这两个tensor相等的元素，相等则返回True，否则返回False
 def tf_equal(sess):
     #普通用法
     a = tf.constant([[3,3,4,1,2,2]])
     b = tf.constant([[1,3,4,3,2,5]])
     c = tf.equal(a, b)
     print("tf.equal(a,b)=\n", sess.run(tf.cast(c, dtype=tf.int32)))
-
     #高级用法
     x = tf.constant([[3],[3],[4],[1],[2],[2]])
     z = tf.equal(x, tf.transpose(x)) #inbatch_softmax修复有这个代码
     print("x=\n", sess.run(x))
     print("tf.equal(x, x.T)=\n", sess.run(tf.cast(z, dtype=tf.int32)))
+
+
 
 def main():
     with tf.Session() as sess:
